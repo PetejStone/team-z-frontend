0795b20ebd8d9f33e48df877560a5d826d6dfb2b
PetejStone <petejstoney@gmail.com>
2019-11-18 10:44:39 -0800

    initial commit

--- team-z-fe/src/App.js
+++ team-z-fe/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
-
+import './styles/App.scss';
+import Game from './components/game/Game'
 const App = () => {
   return (
     <Router>
       <div className="App">
-        <>
-          {/* Route paths for each components */}
-        </>
+      
+        
+          
+            <Game />
+          
+        
       </div>
     </Router>
   )
---
+++ team-z-fe/src/components/game/Game.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+function Game() {
+
+    useEffect(()=> {
+        axios.get('https://lambda-mud-test.herokuapp.com/api/adv/init')
+        .then( res => {
+            console.log(res)
+        })
+
+    }, [])
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default Game
